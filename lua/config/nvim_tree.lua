a88fa2eaacb7685d74d6a368396c0314615788f0
Taylor Whitton <taylorw@knowbe4.com>
2023-02-27 16:56:03 -0500

    Update alpha ascii art

--- lua/config/nvim_tree.lua
+++ lua/config/nvim_tree.lua
@@ -1,7 +1,9 @@
 local nvim_tree = require('nvim-tree')
 local opts = { silent = true, remap = true }
 
-nvim_tree.setup()
+nvim_tree.setup {
+    sync_root_with_cwd = true,
+}
 
 vim.keymap.set('n', '<space>nt', function() nvim_tree.toggle() end, opts)
 vim.keymap.set('n', '<space>nf', function() nvim_tree.focus() end, opts)
