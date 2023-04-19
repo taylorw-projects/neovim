720afc95d40e46dd436a1b05678915e866ca0071
Taylor Whitton <taylorw@knowbe4.com>
2023-04-19 10:29:19 -0400

    Improved hotkeys for nvimtree to open file in tree <space>no

--- lua/config/nvim_tree.lua
+++ lua/config/nvim_tree.lua
@@ -2,13 +2,12 @@ local nvim_tree = require 'nvim-tree'
 local opts = { silent = true, remap = true }
 
 nvim_tree.setup {
-    sync_root_with_cwd = true,
-    respect_buf_cwd = true,
-
+	sync_root_with_cwd = true,
+	respect_buf_cwd = true,
 }
 
 
 vim.keymap.set('n', '<space>nt', function() vim.cmd('NvimTreeToggle') end, opts)
-vim.keymap.set('n', '<space>no', function() vim.cmd('NvimTreeFocus') end, opts)
-vim.keymap.set('n', '<space>nf', function() vim.cmd('NvimTreeFindFile') end, opts)
+vim.keymap.set('n', '<space>nf', function() vim.cmd('NvimTreeFocus') end, opts)
+vim.keymap.set('n', '<space>no', function() vim.cmd('NvimTreeFindFile') end, opts)
 vim.keymap.set('n', '<space>\'', function() vim.cmd('NvimTreeToggle') end, opts)
